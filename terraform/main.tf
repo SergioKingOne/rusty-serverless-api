51b13f4bdb5b96a5fa4b4a83669eebe49abe7fd1
Sergio <sergiorobayorr@gmail.com>
2024-11-03 21:01:36 -0500

    stage name

--- terraform/main.tf
+++ terraform/main.tf
@@ -113,5 +113,18 @@ resource "aws_api_gateway_deployment" "deployment" {
   ]
 
   rest_api_id = aws_api_gateway_rest_api.rusty_api.id
-  stage_name  = "prod"
+
+  stage_name = "prod"
+
+  triggers = {
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.resource.id,
+      aws_api_gateway_method.method.id,
+      aws_api_gateway_integration.integration.id
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
