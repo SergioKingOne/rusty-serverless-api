8029f1b446486a2fac179fa8a39b148faa4207a2
Sergio <sergiorobayorr@gmail.com>
2024-11-04 20:52:27 -0500

    items api endpoint

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,8 @@ output "api_endpoint" {
   description = "API Gateway endpoint URL"
   value       = aws_api_gateway_deployment.deployment.invoke_url
 }
+
+output "items_endpoint" {
+  description = "Items endpoint URL"
+  value       = "${aws_api_gateway_deployment.deployment.invoke_url}${aws_api_gateway_resource.resource.path}"
+}
